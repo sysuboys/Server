16d82228ab4eda86bdea43cac4b653be73237afa
ClimberPG <449063066@163.com>
2017-06-10 22:05:45 +0800

    succeeded in connection

--- dairyu/src/main/java/org/sysuboys/diary/web/websocket/WebSocketHandler.java
+++ dairyu/src/main/java/org/sysuboys/diary/web/websocket/WebSocketHandler.java
@@ -1,16 +1,15 @@
 package org.sysuboys.diary.web.websocket;
 
-import org.springframework.web.socket.TextMessage;  
-import org.springframework.web.socket.WebSocketSession;  
-import org.springframework.web.socket.handler.TextWebSocketHandler;  
-  
-public class WebSocketHandler extends TextWebSocketHandler {  
-  
-    @Override  
-    protected void handleTextMessage(WebSocketSession session,  
-            TextMessage message) throws Exception {  
-        super.handleTextMessage(session, message);  
-        TextMessage returnMessage = new TextMessage(message.getPayload()+" received at server");  
-        session.sendMessage(returnMessage);  
-    }  
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class WebSocketHandler extends TextWebSocketHandler {
+
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		super.handleTextMessage(session, message);
+		TextMessage returnMessage = new TextMessage(message.getPayload() + " received at server");
+		session.sendMessage(returnMessage);
+	}
 }
